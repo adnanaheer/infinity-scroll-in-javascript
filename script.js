c052419dfa75b093ef6f432386aad34038904328
Adnan Aheer <adnanaheer20@gmail.com>
2021-07-08 11:53:19 +0500

    fetching data

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+//  unsplash api
+const count = 10
+const apiKey = 'API_KEY'
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// get photo from unsplash api
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        
+    } catch (error) {
+        // catch error here
+    }
+}
+
+//  on loaded
+getPhotos()
